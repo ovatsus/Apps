096568cef0e7cfcfb3ae828f8c3da411d8ff9a86
Gustavo Guerra <gustavo@codebeside.org>
2013-10-11 22:53:09 +0100

    AdDuplex

--- UKTrains.WP8/Converters.cs
+++ UKTrains.WP8/Converters.cs
@@ -12,14 +12,18 @@ namespace UKTrains
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var distance = (double)value;
-            if (Settings.GetBool(Setting.UseMilesInsteadOfKMs))
+            try
             {
-                return string.Format("{0,1:F1} mi", distance * 0.621371192);
+                if (Settings.GetBool(Setting.UseMilesInsteadOfKMs))
+                {
+                    return string.Format("{0,1:F1} mi", distance * 0.621371192);
+                }
             }
-            else
+            catch
             {
-                return string.Format("{0,1:F1} km", distance);
+                // crashes in the VS preview when trying to get the setting
             }
+            return string.Format("{0,1:F1} km", distance);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
